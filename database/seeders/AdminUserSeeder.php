34f1847194dbfb3462326784e09123a90e075c99
its-sarath28 <sarathrs220@gmail.com>
2024-05-06 14:30:25 +0530

    Added database seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => ' ',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('Password'),
+            'role' => 'ADMIN'
+        ]);
+    }
+}
